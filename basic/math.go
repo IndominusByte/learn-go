3a69e1ac445b31c7b3fc5d5b9b6029ae31bddfb2
NyomanPradipta <pradipta.nyoman@tokopedia.com>
2021-12-22 13:13:50 +0800

    update math

--- basic/math.go
+++ basic/math.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	fmt.Println(math.Round(1.7))
-	fmt.Println(math.Round(1.3))
-	fmt.Println(math.Floor(1.8))
-	fmt.Println(math.Ceil(1.3))
-	fmt.Println(math.Abs(-2))
+	fmt.Println(math.Round(1.7)) // 2
+	fmt.Println(math.Round(1.3)) // 1
+	fmt.Println(math.Floor(1.8)) // 1
+	fmt.Println(math.Ceil(1.3))  // 2
+	fmt.Println(math.Abs(-2))    // 2
 }
